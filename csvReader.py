6ffc94145453220010caf503795cabfed904d668
Concombrero <boyer.tim.travail@gmail.com>
2024-04-29 21:39:36 +0200

    data From CSV

---
+++ csvReader.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+def csvToSortedList(fileName: str):
+    studentList=[]
+    with open(fileName, 'r') as file:
+        #Extraction des donnée
+        data=file.readlines()
+        for studentData in data:
+            studentData=studentData.split(',')
+            studentINE=studentData[0]
+            studentGrade=studentData[1]
+            studentWishList=studentData[2:]
+            
+            #Enlever les retours à la lignes éventuel
+            for i in range(len(studentWishList)):
+                studentWishList[i] = studentWishList[i].replace("\n", "")
+            while '' in studentWishList:
+                studentWishList.remove('')
+            
+            #Crer le profil étudiant et le rajoute à la liste               
+            student={'INE': studentINE, 'grade': studentGrade, 'wishList': studentWishList, 'givenSchool': None}
+            studentList.append(student)
+    
+    #Trie la liste en fonction des grades
+    studentList.sort(key=lambda student: student['grade'], reverse=True)
+    return studentList
+
+def csvToEcole(fileName):
+    schoolList=[]    
+    with open(fileName, 'r') as file:
+        #Extraction des donnée
+        data=file.readlines()
+        for schoolData in data:
+            schoolData=schoolData.split(',')
+            schoolName=schoolData[0]
+            schoolPlaces=schoolData[1]
+            
+            #Crer le profil école et le rajoute à la liste               
+            school={'name': schoolName, 'placesNumber': schoolPlaces, 'remainingPlaces': schoolPlaces}
+            schoolList.append(school)
